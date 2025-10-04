b2886c2022775e43f87824849560ca1683331e6f
ffff-22 <fukiinukai0720@outlook.jp>
2025-10-04 19:46:45 +0900

    Create script.js

---
+++ password_generator/script.js
@@ -0,0 +1,26 @@
+// script.js
+function generatePassword() {
+  const length = document.getElementById("length").value;
+  const includeUppercase = document.getElementById("includeUppercase").checked;
+  const includeLowercase = document.getElementById("includeLowercase").checked;
+  const includeNumbers = document.getElementById("includeNumbers").checked;
+  const includeSymbols = document.getElementById("includeSymbols").checked;
+
+  let charset = "";
+  if (includeUppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (includeLowercase) charset += "abcdefghijklmnopqrstuvwxyz";
+  if (includeNumbers) charset += "0123456789";
+  if (includeSymbols) charset += "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+  if (charset === "") {
+    alert("少なくとも1つの文字種を選んでください");
+    return;
+  }
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+
+  document.getElementById("result").value = password;
+}
